8333ea77def8438f461ec98d7cc63d40a61d7eab
ElenaAskarova <askarova1205@mail.ru>
2022-11-22 16:28:59 +0500

    First_task

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# links #
+
+Solution Task002\Program.cs
+
+Diagram Task002\diagram_task002.drawio.png
---
+++ Task002/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine("Большее число:");
+    Console.Write(num1);
+    Console.WriteLine("Меньшее число:");
+    Console.Write(num2);
+}
+else
+{
+    Console.Write("Большее число:");
+    Console.WriteLine(num2);
+    Console.Write("Меньшее число:");
+    Console.WriteLine(num1);
+}
